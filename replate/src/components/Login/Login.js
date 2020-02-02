a9072bbf4fb656b42fd3fbe7a5142189ef1a1301
Germaine Pannell <me@germainep.com>
2020-02-02 17:27:18 -0500

    update to password field

--- replate/src/components/Login/Login.js
+++ replate/src/components/Login/Login.js
@@ -5,25 +5,27 @@ import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 // create a component
 const Login = () => {
 
-  const [user, setUser] = useState({username: '', password: ''})
+  const [user, setUser] = useState({email: '', password: ''})
 
   const handleChange = event => {
     setUser({...user, [event.target.name]: event.target.value});
+    console.log(user);
   }
 
   const handleSubmit = event => {
     event.preventDefault();
+    console.log(user);
     
   }
 
  return (
   <Container maxWidth='xs'>
    <h1>Login</h1>
-   <form>
+   <form onSubmit={event => handleSubmit(event)}>
 
-    <TextField label='Email' type='email' required fullWidth name='email' id='email' variant='filled' margin='normal' onChange={event=> handleChange(event)}></TextField>
+    <TextField label='Email' type='email' required fullWidth name='email' id='email' variant='filled' margin='normal' onChange={event => handleChange(event)}></TextField>
 
-    <TextField label='Password' type='password' required fullWidth name='password' id='password' variant='filled' margin='normal'></TextField>
+    <TextField label='Password' type='password' required fullWidth name='password' id='password' variant='filled' margin='normal' onChange={event => handleChange(event)}></TextField>
 
     <Button type='submit' fullWidth color='primary' variant='contained'>Submit</Button>
    </form>
