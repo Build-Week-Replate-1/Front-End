cec3f0cf342ceabacef0e21cbc86b9042f392b18
Germaine Pannell <me@germainep.com>
2020-02-02 21:32:49 -0500

    added links for registration

--- replate/src/components/Login/Login.js
+++ replate/src/components/Login/Login.js
@@ -1,12 +1,12 @@
 //import liraries
 import React, { useState, useEffect } from 'react';
-import {Container, TextField, Button }  from '@material-ui/core';
+import {Container, TextField, Button, Link, Grid }  from '@material-ui/core';
 import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import axios from 'axios';
 // create a component
 const Login = () => {
 
-  const [user, setUser] = useState({email: '', password: ''})
+  const [user, setUser] = useState({username: '', password: ''})
 
   const handleChange = event => {
     setUser({...user, [event.target.name]: event.target.value});
@@ -17,29 +17,61 @@ const Login = () => {
     event.preventDefault();
     console.log(user);
     axios
-    .post('#', user)
-    .then(res => {
-      console.log(res.data);
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .post("https://replated.herokuapp.com/api/volunteer/login", user)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
     
   }
 
  return (
-  <Container maxWidth='xs'>
-   <h1>Login</h1>
-   <form onSubmit={event => handleSubmit(event)}>
+   <Container maxWidth="xs">
+     <h1>Login</h1>
+     <form onSubmit={event => handleSubmit(event)}>
+       <TextField
+         label="Username"
+         type="text"
+         required
+         fullWidth
+         name="username"
+         id="username"
+         value={user.username}
+         variant="filled"
+         margin="normal"
+         onChange={event => handleChange(event)}
+       />
 
-    <TextField label='Email' type='email' required fullWidth name='email' id='email' value={user.email} variant='filled' margin='normal' onChange={event => handleChange(event)}></TextField>
+       <TextField
+         label="Password"
+         type="password"
+         required
+         fullWidth
+         name="password"
+         id="password"
+         value={user.password}
+         variant="filled"
+         margin="normal"
+         onChange={event => handleChange(event)}
+       />
 
-    <TextField label='Password' type='password' required fullWidth name='password' id='password' value={user.password} variant='filled' margin='normal' onChange={event => handleChange(event)}></TextField>
-
-    <Button type='submit' fullWidth color='primary' variant='contained'>Submit</Button>
-   </form>
-
-  </Container>
+       <Button type="submit" fullWidth color="primary" variant="contained">
+         Submit
+       </Button>
+     </form>
+     <Grid container justify='center' spacing={3}>
+       <Grid item>
+         <Link color="secondary">
+           Business Registration
+         </Link>
+       </Grid>
+       <Grid item>
+       <Link color="secondary">Volunteer Registration</Link>
+       </Grid>
+     </Grid>
+   </Container>
  );
 };
 
