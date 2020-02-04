9de4b709e5d6d957328f1e52d0184cc2b729ffde
Germaine Pannell <me@germainep.com>
2020-02-03 20:40:34 -0500

    Started file for business registration

---
+++ replate/src/components/Registration/BusinessRegistaion.js
@@ -0,0 +1,15 @@
+//import liraries
+import React, { useState, useEffect } from 'react';
+
+// create a component
+const BusinessRegistration = () => {
+ return (
+  <div>
+    
+  </div>
+ );
+};
+
+
+//make this component available to the app
+export default BusinessRegistration;
